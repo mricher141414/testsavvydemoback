5ac9bd6cb73c3d838137c4e9ba5aa76cc14c9597
Kitty <Kate.Phaneuf-Michaud@cgi.com>
2019-05-21 16:19:40 -0400

    PSHE-235 changement de .bat dois apeler un java (java non complet)

--- Calculation.bat
+++ Calculation.bat
@@ -1,7 +1,2 @@
 echo off
-sqlcmd -U AutomationAdmin -P TestSavvyAdmin!4 -S timesheetcgi.database.windows.net -d timesheetdb -Q "SELECT SUM(Id) FROM dbo.Role" -o output.txt
-set /p delExit=Press the ENTER key to exit...:
-SELECT employeeid, PAY FROM (
-//(SELECT salary FROM (left joint(left joint queue + timesheet) + employee)*
-//(SELECT value FROM (left joint(left joint (left join queue + timesheet) + timesheetRow) + hour_project))
-//AS PAY)
+java -jar Salary_Calculation.jar
